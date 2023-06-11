708ab468020322a155b1e32417abbc7f164a5193
subAngel <whoangel.agl@gmail.com>
2023-06-11 02:27:25 -0600

    style(home): creando un column grid para las cards de los proyectos"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,25 @@
 <template>
-	<div class="flex items-center h-screen w-screen">
+	<div class="flex items-center h-screen">
 		<div class="hero min-h-screen mx-auto">
 			<div class="hero-content text-center">
 				<div class="max-w-md">
 					<h1 class="text-5xl font-bold my-9">50 proyectos en 50 dias</h1>
-
-					<div
-						class="my-28 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-10 w-screen"
-					>
-						<RouterLink
-							to="/expanding-cards"
-							class="card w-52 rounded-sm shadow-xl"
-						>
-							<div class="card-body">
-								<h2 class="card-title">Expanding Cards</h2>
-								<p>Imagen aqui</p>
-							</div>
-						</RouterLink>
-
-						<div class="card w-52 rounded-sm bg-base-100 shadow-xl">
-							<div class="card-body">
-								<h2 class="card-title">Card</h2>
-								<p>Imagen aqui</p>
-							</div>
-						</div>
-					</div>
 				</div>
+			</div>
+		</div>
+	</div>
+	<div class="my-28 grid grid-cols-4 gap-4 mx-36">
+		<RouterLink to="/expanding-cards" class="card w-52 rounded-sm shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Expanding Cards</h2>
+				<p>Imagen aqui</p>
+			</div>
+		</RouterLink>
+
+		<div class="card w-52 rounded-sm bg-base-100 shadow-xl">
+			<div class="card-body">
+				<h2 class="card-title">Card</h2>
+				<p>Imagen aqui</p>
 			</div>
 		</div>
 	</div>
